9d75edadd48b7970ec88b3f65e51d65790f55ef2
Khalil Al-Rashid <rashedki@hotmail.com>
2020-06-13 13:57:38 +0300

    Creating a GET endpoint to get questions based on their category.

--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -173,7 +173,25 @@ def create_app(test_config=None):
   categories in the left column will cause only questions of that
   category to be shown.
   '''
+  @app.route('/categories/<int:category_id>/questions', methods=['GET'])
+  def get_questions_by_category(category_id):
+      try:
+          questions = Question.query.filter(
+              Question.category == category_id).all()
 
+          if(questions is None):
+              abort(404)
+
+          formatted_questions = [question.format() for question in questions]
+
+          return jsonify({
+              'success': True,
+              'questions': formatted_questions,
+              'total_questions': len(formatted_questions)
+          })
+
+        except:
+             abort(422)
 
   '''
   @TODO:
