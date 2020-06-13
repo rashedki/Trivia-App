4150cb7143a20cccd84e7275d3603e24452bd695
Khalil Al-Rashid <rashedki@hotmail.com>
2020-06-13 10:51:59 +0300

    Fixing the issue of connecting to the server
    This is another way of connecting my app to my server...

--- backend/models.py
+++ backend/models.py
@@ -3,8 +3,9 @@ from sqlalchemy import Column, String, Integer, create_engine
 from flask_sqlalchemy import SQLAlchemy
 import json
 
+#This is another way to connect to the db
 database_name = "trivia"
-database_path = "postgres://khalil".format('localhost:5432', database_name)
+database_path = "postgres://{}:{}@{}/{}".format('postgres', 'khalil', 'localhost:5432', database_name)
 
 db = SQLAlchemy()
 
