efd8a516b940b4615fc233526ae244c5beb45ba7
Khalil Al-Rashid <rashedki@hotmail.com>
2020-06-16 20:21:19 +0300

    Adding the database path and the new question

--- backend/test_flaskr.py
+++ backend/test_flaskr.py
@@ -15,8 +15,15 @@ class TriviaTestCase(unittest.TestCase):
         self.app = create_app()
         self.client = self.app.test_client
         self.database_name = "trivia_test"
-        self.database_path = "postgres://{}/{}".format('localhost:5432', self.database_name)
+        self.database_path = "postgres://{}:{}@{}/{}".format('postgres', 'khalil', 'localhost:5432', database_name)
         setup_db(self.app, self.database_path)
+
+        self.new_question = {
+            'question': 'New Test Question',
+            'answer': 'Test Answer',
+            'difficulty': 1,
+            'category': 1
+        }
 
         # binds the app to the current context
         with self.app.app_context():
@@ -24,7 +31,7 @@ class TriviaTestCase(unittest.TestCase):
             self.db.init_app(self.app)
             # create all tables
             self.db.create_all()
-    
+
     def tearDown(self):
         """Executed after reach test"""
         pass
